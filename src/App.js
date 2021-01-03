59d007ee01040b8f5ef3143fb07fcc04f4759c97
Mohammed Motar <mohammed.motar.mm@gmail.com>
2021-01-03 17:02:45 +0100

    working on main page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Main from "./view/Main";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Router>
+        <Route exact path="/" component={Main} />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
