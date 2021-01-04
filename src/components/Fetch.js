52adb908507beb3fa03a11c0872d72cc69aa633e
Mohammed Motar <mohammed.motar.mm@gmail.com>
2021-01-04 16:52:13 +0100

    building component for PC components

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -4,14 +4,17 @@ import PcComponent from "./PcComponent";
 
 function Fetch({ fetchUrl }) {
   const [data, setData] = useState([]);
+  // console.log(data[1]);
 
   useEffect(() => {
     async function fetchData() {
       const request = await axios(fetchUrl ?? "components");
-      console.log(request?.config?.url);
+      // console.log(request?.config?.url);
       if (request.config.url === "components") {
         const data = request?.data[0];
+        // console.log(data);
         let arrData = [];
+        let arr = [];
 
         arrData.push(
           data?.cpus,
@@ -22,6 +25,18 @@ function Fetch({ fetchUrl }) {
           data?.chassis,
           data?.cpu_coolings
         );
+
+        data.chassis.map((x) => x).map((x) => arr.push(x));
+        data.cpu_coolings.map((x) => x).map((x) => arr.push(x));
+        data.cpus.map((x) => x).map((x) => arr.push(x));
+        data.gpus.map((x) => x).map((x) => arr.push(x));
+        data.harddrives.map((x) => x).map((x) => arr.push(x));
+        data.motherboards.map((x) => x).map((x) => arr.push(x));
+        data.psus.map((x) => x).map((x) => arr.push(x));
+        data.rams.map((x) => x).map((x) => arr.push(x));
+        // chassis.map((x) => arr.push(x));
+
+        console.log(arr);
 
         setData(arrData);
       } else {
@@ -35,6 +50,9 @@ function Fetch({ fetchUrl }) {
 
   return (
     <div>
+      {data?.map((pc) => {
+        // console.log(pc);
+      })}
       <PcComponent />
     </div>
   );
